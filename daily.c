32eaeef8e3a9dc7ddb6f4102d2edacee75804f64
Anshman Khare <anshmankhare@gmail.com>
2025-09-15 22:16:48 +0530

    more

--- daily.c
+++ daily.c
@@ -553,16 +553,25 @@
 
 
 // print all the letter in english alphabet using pointer 
+// int main() {
+//   int a[26];
+//   int *ptr=a;
+//       for (int i = 0; i < 26; i++) {
+//         *(ptr + i) = 'A' + i;
+//     }
+//    for (int i = 0; i < 26; i++) {
+//         printf("%c \n", *(ptr + i));
+//     }
+//     return 0;
+// }
+
+// write a program to enter price of 3 items and print their final cost with gst using array
 int main() {
-  int a[26];
-  int *ptr=a;
-      for (int i = 0; i < 26; i++) {
-        *(ptr + i) = 'A' + i;
-    }
-   for (int i = 0; i < 26; i++) {
-        printf("%c \n", *(ptr + i));
-    }
+    int price[3];
+    printf("Enter 3 price: ");
+    scanf("%d %d %d",&price[0],&price[1],&price[2]);
+    printf("Price of 1 = %f\n",price[0]+(0.18*price[0]));
+    printf("Price of 2 = %f\n",price[1]+(0.18*price[1]));
+    printf("Price of 3 = %f\n",price[2]+(0.18*price[2]));
     return 0;
 }
-
-// write a program to enter price of 3 items and print their final cost with gst
