5d81c29a0dbc863c7dbc5793a4feeb7f523d7653
Anshman Khare <anshmankhare@gmail.com>
2025-09-16 22:56:37 +0530

    day 7

--- daily.c
+++ daily.c
@@ -594,3 +594,36 @@
 //   }
 //   return count;
 // }
+
+// write a function to reverse an array
+#include <stdio.h>
+
+void rev(int arr[], int n);
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter array values:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    rev(arr, n);
+
+    printf("Reversed array:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+void rev(int arr[], int n) {
+    for(int i = 0; i < n/2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+}
