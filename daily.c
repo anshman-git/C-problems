dbd07a05ca149274abbbc2996f6ffd9f9dba28e8
Anshman Khare <anshmankhare@gmail.com>
2025-09-19 22:51:28 +0530

    day 10

--- daily.c
+++ daily.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include<math.h>
+#include<string.h>
 
 // Day 1
 
@@ -690,17 +691,29 @@
 // }
 
 // make a program that input user name and print its length
-int countLength(char arr[]);
+// int countLength(char arr[]);
+// int main() {
+//     char name[100];
+//     fgets(name,100,stdin);
+//     printf("length = %d",countLength(name));
+//     return 0;
+// }
+// int countLength(char arr[]) {
+//     int count = 0;
+//     for(int i=0;arr[i]!='\0';i++) {
+//         count++;
+//     }
+//     return count-1;
+// }
+
+
+// make a program that input user name and print its length using string
 int main() {
-    char name[100];
-    fgets(name,100,stdin);
-    printf("length = %d",countLength(name));
-    return 0;
-}
-int countLength(char arr[]) {
-    int count = 0;
-    for(int i=0;arr[i]!='\0';i++) {
-        count++;
-    }
-    return count-1;
+    char a[100];
+   
+    printf("Enter the charectar to check the length : ");
+    scanf(" %s",a); 
+    int length = strlen(a);
+    printf("length = %d",length);
+
 }
