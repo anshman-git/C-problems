ef545e0ac3a3f71c6d95fd126ae7a26200f98fd9
Anshman Khare <anshmankhare@gmail.com>
2025-09-29 22:12:59 +0530

    day 20

--- daily.c
+++ daily.c
@@ -1183,3 +1183,20 @@
 // }
 
 
+// reverse an array using pointer
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *start = arr,*end = arr + 4,temp;
+    while (start < end) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
