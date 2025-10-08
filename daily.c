d5d78be1856d2a75ef470a3374243be71a65c3c7
Anshman Khare <anshmankhare@gmail.com>
2025-10-08 23:21:58 +0530

    day 28

--- daily.c
+++ daily.c
@@ -1556,3 +1556,68 @@
 
 //     return 0;
 // }
+
+
+// 81
+// Allocate memory to store first 5 odd number, then reallocate it top store first 6 even number.
+// int main() {
+//     int *ptr;
+//     ptr = (int *) calloc(5,sizeof(int));
+//     ptr[0]=1;
+//     ptr[1]=2;
+//     ptr[2]=3;
+//     ptr[3]=4;
+//     ptr[4]=5;
+//     for (int i=0;i<5;i++) {
+//         printf("%d\n",ptr[i]);
+//     }
+//     printf("\n");
+//     ptr[0] = 2;
+//     ptr[1] =4;
+//     ptr[2] =6;
+//     ptr[3] =8;
+//     ptr[4] =12;
+//     ptr[5] =14;
+//     for(int i=0;i<6;i++) {
+//         printf("%d\n",ptr[i]);
+//     }
+//     return 0;
+    
+// }
+
+// 82
+// Allocate memory for 5 no then dynamically increase to 8 no.
+// int main() {
+//     int *ptr;
+//     ptr = (int *) calloc(5,sizeof(int));
+//     printf("Enter number(5):");
+//     for(int i=0;i<5;i++) {
+//         scanf("%d",&ptr[i]);
+//     }
+//     ptr = realloc(ptr,8);
+//     printf("Enter Numbers(8):");
+//     for(int i=0;i<8;i++) {
+//         scanf("%d",&ptr[i]);
+//     }
+//     for(int i=0;i<8;i++) {
+//         printf("Number %d is %d\n",i,ptr[i]);
+//     }
+//     return 0;
+// }
+
+// 83
+// Create an array of size 5(using calloc) &enter its value from the user
+// int main() {
+//     int *ptr;
+//     ptr = (int *) calloc(5,sizeof(int));
+//     printf("Enter numbers(5):");
+//     for(int i=0;i<5;i++) {
+//         scanf("%d",&ptr[i]);
+//     }
+
+//     for(int i=0;i<5;i++) {
+//         printf("Number %d is %d\n",i,ptr[i]);
+//     }
+//     return 0;
+
+// }
