c61a399e8306dc61842baf7762612b59c96c9ae7
Anshman Khare <anshmankhare@gmail.com>
2025-10-18 23:23:37 +0530

    day 39

--- daily.c
+++ daily.c
@@ -1940,3 +1940,50 @@
 
 
 // 98
+// hacker rank question for printing sum of marks odd for girls and even for boys using array and function
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+// int marks_summation(int* marks, int number_of_students, char gender) {
+//   int a=0;
+//   if(gender=='b') {
+//     for(int i=0;i<number_of_students;i=i+2){
+//     a=a+marks[i];
+//     }
+//     return a;
+//   }
+//   else if(gender =='g') {
+//     for(int i=1;i<number_of_students;i=i+2){
+//     a=a+marks[i];
+//   }
+//   return a;
+// }
+// return a;
+// }
+
+// int main() {
+//     int number_of_students;
+//     char gender;
+//     int sum;
+  
+//     scanf("%d", &number_of_students);
+//     int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+//     for (int student = 0; student < number_of_students; student++) {
+//         scanf("%d", (marks + student));
+//     }
+    
+//     scanf(" %c", &gender);
+//     sum = marks_summation(marks, number_of_students, gender);
+//     printf("%d", sum);
+//     free(marks);
+ 
+//     return 0;
+// }
+
+
+// 99
