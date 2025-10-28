fbb52ae45af6407468f9b57ff45630885a293fbe
Anshman Khare <anshmankhare@gmail.com>
2025-10-28 22:40:22 +0530

    project

--- student_academic_information.c
+++ student_academic_information.c
@@ -1,3 +1,4 @@
+//it is difficult to make this project so i decide to make 1 by 1 function like any company provides updates and feachers i will upload new functions.
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -14,57 +15,16 @@ struct Student {
 };
 
 void addStudent(struct Student stu[]);
-void displayAll(struct Student stu[], int count);
-int searchStudent(struct Student stu[], int count, int roll);
-void updateStudent(struct Student stu[], int count);
-void deleteStudent(struct Student stu[], int *count);
-void generateReportCard(struct Student stu[], int index);
-int saveToFile(struct Student stu[], int count);
-int loadFromFile(struct Student stu[], int *count);
-
-
-struct Student *stu;
-int count = 0;
-int capacity = 10;
 
 int main() {
-    int choice;
-    struct Student *stu = malloc(10 * sizeof(struct Student));
-    stu = realloc(stu, 20* sizeof(struct Student));
-  
+        int choice;
     printf("\t\tUnited Group Of Institute\n");
     printf("Choose the function:-\n");
     while(1){
     printf("1.Add Student\n");
-    printf("2.Display All\n");
-    printf("3.Search Student\n");
-    printf("4.Update Student\n");
-    printf("5.Delete Student\n");
-    printf("6.Generate Report Card\n");
-    printf("7.Save To File\n");
-    printf("8.Load from file\n");
-    printf("9.Exit\n");
     scanf("%d",&choice);
     switch(choice){
         case 1: addStudent();
-                break;
-        case 2: displayAll(); 
-                break;
-        case 3: printf("Enter roll no.:");
-                scanf("%d",&stu.roll);
-                searchStudent(stu.roll); 
-                break;
-        case 4: updateStudent(); 
-                break;
-        case 5: deleteStudent(); 
-                break;
-        case 6: generateReportCard(); 
-                break;
-        case 7: savetofile(); 
-                break;
-        case 8: loadfromfile(); 
-                break;
-        case 9: exit(0); 
                 break;
         default: printf("invalid choice"); 
     }
