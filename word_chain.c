a4860a4049659ac198af9aeb2ca03b1c152f5615
Anshman Khare <anshmankhare@gmail.com>
2025-11-21 00:06:42 +0530

    game

--- word_chain.c
+++ word_chain.c
@@ -2,6 +2,7 @@
 #include<string.h>
 void one_player_game();
 void two_player_game();
+void n_player_game();
 void rules();
 
 int main() {
@@ -9,12 +10,14 @@ int main() {
     printf("\t \t Welcome to the Word Chain game\n");
     printf("\n");
     rules();
-    printf("\n1.One Player \n2.Two player\n");
+    printf("\n1.One Player \n2.Two player \n3.Multiplayer player\n");
     scanf("%d",&op);
    switch (op) {
    case 1:one_player_game();
         break;
-   case 2:two_player_game();
+    case 2:two_player_game();
+        break;
+   case 3:n_player_game();
         break;
    default:printf("Wrong option");
     break;
@@ -52,25 +55,82 @@ void rules() {
     printf("\n");
 }
 
-void two_player_game() {
+void n_player_game() {
     char word[100];
     int length;
     char first_word;
     char last_word='a';
-    
+    int n;
+    printf("Enter number of players...");
+    scanf("%d",&n);
     printf("Game Started\n\n");
+    do{
+    for(int i=1;i<=n;i++) {
+
     do {
-        printf("Player 1\n");
+        printf("Player %d\n",i);
         printf("Enter a word starting with %c ",last_word);
         scanf("%s",word);
         length=strlen(word);
         first_word=word[0];
         if(first_word!=last_word) {
+            goto a;
+        }
+        if(first_word==last_word) {
+            last_word=word[length-1];
             break;
         }
-        last_word=word[length-1];
         
     } while(1);
-
+    }
+}while(1);
+    a:
     printf("\nxxxx Wrong word xxxx\n");
+}
+
+void two_player_game() {
+
+    char word[100];
+    int length;
+    char first_word;
+    char last_word='a';
+    printf("Game Started\n\n");
+    
+    int player;
+    do {
+        player=1;
+        b:
+    if(player==1){
+        printf("player %d enter a word starts with %c :",player,last_word);
+        scanf("%s",word);
+        length=strlen(word);
+        first_word=word[0];
+        if(first_word!=last_word) {
+            goto a;
+        }
+        else if(first_word==last_word) {
+        last_word=word[length-1];
+        player=2;
+        goto b;
+        }
+    }
+    else if(player==2){
+        printf("player %d enter a word start with %c: ",player,last_word);
+        scanf("%s",word);
+         length=strlen(word);
+        first_word=word[0];
+         if(first_word!=last_word) {
+            goto a;
+        }
+            last_word=word[length-1];
+          
+        player=1;
+    }
+        
+    } while(1);
+a:
+printf("\nxxxx Wrong word xxxx\n");
+printf("Player %d loss\n",player);
+
+    
 }
