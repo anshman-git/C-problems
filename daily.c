1b08fbf37e21db89f06bb10f09311f686f6597d7
Anshman Khare <anshmankhare@gmail.com>
2025-12-29 23:27:56 +0530

    ques 110

--- daily.c
+++ daily.c
@@ -2309,49 +2309,68 @@
 // }
 
 // 109 3D array multipication
-int main() {
-    int r1, c1, r2, c2, i, j, k;
-    int a[10][10], b[10][10], res[10][10];
+// int main() {
+//     int r1, c1, r2, c2, i, j, k;
+//     int a[10][10], b[10][10], res[10][10];
 
-    printf("Enter rows and columns for first matrix: ");
-    scanf("%d %d", &r1, &c1);
-    printf("Enter rows and columns for second matrix: ");
-    scanf("%d %d", &r2, &c2);
+//     printf("Enter rows and columns for first matrix: ");
+//     scanf("%d %d", &r1, &c1);
+//     printf("Enter rows and columns for second matrix: ");
+//     scanf("%d %d", &r2, &c2);
 
-    if (c1 != r2) {
-        printf("Error! Column of first matrix not equal to row of second.\n");
-        return 0;
-    }
+//     if (c1 != r2) {
+//         printf("Error! Column of first matrix not equal to row of second.\n");
+//         return 0;
+//     }
 
-    printf("\nEnter elements of matrix 1:\n");
-    for (i = 0; i < r1; i++)
-        for (j = 0; j < c1; j++)
-            scanf("%d", &a[i][j]);
+//     printf("\nEnter elements of matrix 1:\n");
+//     for (i = 0; i < r1; i++)
+//         for (j = 0; j < c1; j++)
+//             scanf("%d", &a[i][j]);
 
-    printf("\nEnter elements of matrix 2:\n");
-    for (i = 0; i < r2; i++)
-        for (j = 0; j < c2; j++)
-            scanf("%d", &b[i][j]);
+//     printf("\nEnter elements of matrix 2:\n");
+//     for (i = 0; i < r2; i++)
+//         for (j = 0; j < c2; j++)
+//             scanf("%d", &b[i][j]);
 
-    for (i = 0; i < r1; i++)
-        for (j = 0; j < c2; j++)
-            res[i][j] = 0;
+//     for (i = 0; i < r1; i++)
+//         for (j = 0; j < c2; j++)
+//             res[i][j] = 0;
 
-    for (i = 0; i < r1; i++) {        
-        for (j = 0; j < c2; j++) {   
-            for (k = 0; k < c1; k++) { 
-                res[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
+//     for (i = 0; i < r1; i++) {        
+//         for (j = 0; j < c2; j++) {   
+//             for (k = 0; k < c1; k++) { 
+//                 res[i][j] += a[i][k] * b[k][j];
+//             }
+//         }
+//     }
 
-    printf("\nResultant Matrix:\n");
-    for (i = 0; i < r1; i++) {
-        for (j = 0; j < c2; j++) {
-            printf("%d  ", res[i][j]);
-        }
-        printf("\n");
-    }
+//     printf("\nResultant Matrix:\n");
+//     for (i = 0; i < r1; i++) {
+//         for (j = 0; j < c2; j++) {
+//             printf("%d  ", res[i][j]);
+//         }
+//         printf("\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+// 110
+// Given a string, remove all consecutive duplicate characters from it in-place, preserving the original order, without using any extra string or array.
+// int main() {
+//     char str[100];
+//     scanf("%s",str);
+//     int l=strlen(str)-1;
+//     int a=1;
+//     char str2[100];
+//     str2[0]=str[0];
+//     for(int i=0;i<=l;i++) {
+//         if(str[i]!=str[i+1]){
+//         str2[a++]=str[i];
+//         }
+//     }
+//     str2[a+1]='\0';
+//     printf("%s",str2);
+//     return 0;
+// }
