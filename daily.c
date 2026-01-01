d1818923215f3e996891feb5ab80ff14dd31b034
Anshman Khare <anshmankhare@gmail.com>
2026-01-01 23:06:29 +0530

    111

--- daily.c
+++ daily.c
@@ -2376,3 +2376,31 @@
 // }
 
 // Happy new year 2026
+
+// 111
+// HACKER RANK QUESTION
+// Given a string s, the objective is to convert it into integer format without utilizing any built-in functions. Refer the below steps to know about atoi() function.
+// int myAtoi(char *s) {
+//     int i = 0, sign = 1;
+//     long result = 0;
+
+//     while (s[i] == ' ') i++;
+
+//     if (s[i] == '-' || s[i] == '+') {
+//         if (s[i] == '-') sign = -1;
+//         i++;
+//     }
+
+//     while (s[i] >= '0' && s[i] <= '9') {
+//         result = result * 10 + (s[i] - '0');
+//         i++;
+//     }
+    
+//      if (sign == 1 && result > INT_MAX)
+//             return INT_MAX;
+//         if (sign == -1 && -result < INT_MIN)
+//             return INT_MIN;
+
+
+//     return result * sign;
+// }
