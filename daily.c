e241e276ba76f401254b437b55c9d646811c0a82
Anshman Khare <anshmankhare@gmail.com>
2026-01-05 23:39:36 +0530

    113

--- daily.c
+++ daily.c
@@ -2511,3 +2511,31 @@
 //   }
 //   return 0;
 // }
+
+// 114
+// Bubble sort
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int data[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(data) / sizeof(data[0]);
+
+    bubbleSort(data, n);
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", data[i]);
+    }
+    return 0;
+}
