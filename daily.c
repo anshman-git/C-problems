df7d25d33bf4a3667b4ed10196b0501bfcd03849
Anshman Khare <anshmankhare@gmail.com>
2026-01-06 23:56:39 +0530

    115

--- daily.c
+++ daily.c
@@ -2539,3 +2539,33 @@
 //     }
 //     return 0;
 // }
+
+// 115
+// Selection sort
+// void selectionSort(int arr[], int n) {
+//     int i, j, min_idx, temp;
+//     for (i = 0; i < n - 1; i++) {
+//         min_idx = i;
+//         for (j = i + 1; j < n; j++) {
+//             if (arr[j] < arr[min_idx]) {
+//                 min_idx = j;
+//             }
+//         }
+//         temp = arr[i];
+//         arr[i] = arr[min_idx];
+//         arr[min_idx] = temp;
+//     }
+// }
+
+// int main() {
+//     int data[] = {64, 34, 25, 12, 22, 11, 90};
+//     int n = sizeof(data) / sizeof(data[0]);
+
+//     selectionSort(data, n);
+
+//     printf("Sorted array: \n");
+//     for (int i = 0; i < n; i++) {
+//         printf("%d ", data[i]);
+//     }
+//     return 0;
+// }
