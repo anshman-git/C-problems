dbe94985ef5df9802725a9e5cba640dd1c225e7e
Anshman Khare <anshmankhare@gmail.com>
2026-01-09 23:54:29 +0530

    ques 117

--- daily.c
+++ daily.c
@@ -2597,3 +2597,55 @@
 //     }
 //     return 0;
 // }
+
+
+
+// 117
+// Merge sort
+void merge(int arr[], int left, int mid, int right) {
+    int i = left, j = mid + 1, k = left;
+    int temp[right + 1];
+    
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+    
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+    
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+    
+    for (int i = left; i <= right; i++) {
+        arr[i] = temp[i];
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int data[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(data) / sizeof(data[0]);
+
+    mergeSort(data, 0, n - 1);
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", data[i]);
+    }
+    return 0;
+}
