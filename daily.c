4a3d824755c71c4172c1dbf54b591630ca1824b1
Anshman Khare <anshmankhare@gmail.com>
2026-01-12 22:44:18 +0530

    ques 120

--- daily.c
+++ daily.c
@@ -2725,3 +2725,43 @@
 //     }
 //     return 0;
 // }
+
+//120
+// Binary Search
+int binarySearch(int arr[], int left, int right, int key) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter sorted array elements: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int key;
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+    
+    int result = binarySearch(arr, 0, n - 1, key);
+    
+    if(result != -1) {
+        printf("Element found at index: %d\n", result);
+    } else {
+        printf("Element not found\n");
+    }
+    return 0;
+}
