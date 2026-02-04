146f5832576e771d8f25cabce8243c5b8020e1cd
Anshman Khare <anshmankhare@gmail.com>
2026-02-04 23:58:42 +0530

    135

--- daily.c
+++ daily.c
@@ -3376,18 +3376,59 @@
 
 // 135
 // "Implement the algorithm to find the next lexicographically greater permutation of an integer array in-place ."
-
-// int main() {
-
-//     int arr[3];
-//     printf("Enter :");
-//     for(int i=0;i<3;i++) {
-//         scanf("%d",&arr[i]);
+ 
+ 
+// void swap(int *a, int *b) {
+//     int temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+ 
+// void reverse(int arr[], int start, int end) {
+//     while (start < end) {
+//         swap(&arr[start], &arr[end]);
+//         start++;
+//         end--;
 //     }
-
-//     int arr_2[][3];
-    
-
 // }
 
-// incomplete...
+// void nextPermutation(int arr[], int n) {
+//     int i = n - 2;
+
+//     while (i >= 0 && arr[i] >= arr[i + 1]) {
+//         i--;
+//     }
+
+//     if (i >= 0) {
+//         int j = n - 1;
+//         while (arr[j] <= arr[i]) {
+//             j--;
+//         }
+//         swap(&arr[i], &arr[j]);
+//     }
+
+//     reverse(arr, i + 1, n - 1);
+// }
+
+// int main() {
+//     int n;
+
+//     printf("Enter size of array: ");
+//     scanf("%d", &n);
+
+//     int arr[n];
+
+//     printf("Enter %d elements:\n", n);
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     nextPermutation(arr, n);
+
+//     printf("Next permutation:\n");
+//     for (int i = 0; i < n; i++) {
+//         printf("%d ", arr[i]);
+//     }
+
+//     return 0;
+// }
