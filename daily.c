fc853d506b8092dac1a4f80487d0d3ef80d53ff5
Anshman Khare <anshmankhare@gmail.com>
2026-02-28 23:58:57 +0530

    146

--- daily.c
+++ daily.c
@@ -3856,3 +3856,14 @@
 
 //     s2[k] = '\0';
 // }
+
+// 146
+// Given an array arr[], check whether it is sorted in non-decreasing order. Return true if it is sorted otherwise false.
+// bool isSorted(int arr[], int n) {
+//     // Code Here
+//     for(int i=1;i<n;i++) {
+//         if(arr[i-1]<=arr[i]) continue;
+//         else return 0;
+//     }
+//     return 1;
+// }
