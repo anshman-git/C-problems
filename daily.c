1583e3822b125adfacf3e4e50d1d4937e6228e3c
Anshman Khare <anshmankhare@gmail.com>
2026-03-01 23:56:20 +0530

    147

--- daily.c
+++ daily.c
@@ -3860,10 +3860,29 @@
 // 146
 // Given an array arr[], check whether it is sorted in non-decreasing order. Return true if it is sorted otherwise false.
 // bool isSorted(int arr[], int n) {
-//     // Code Here
 //     for(int i=1;i<n;i++) {
 //         if(arr[i-1]<=arr[i]) continue;
 //         else return 0;
 //     }
 //     return 1;
 // }
+
+//147
+// Longest Common Prefix 
+// char* longestCommonPrefix(char** strs, int strsSize) {
+//     if (strsSize == 0)
+//         return "";
+
+//     for (int i = 0; strs[0][i] != '\0'; i++) {
+//         char ch = strs[0][i];
+
+//         for (int j = 1; j < strsSize; j++) {
+//             if (strs[j][i] == '\0' || strs[j][i] != ch) {
+//                 strs[0][i] = '\0';
+//                 return strs[0];
+//             }
+//         }
+//     }
+//     return strs[0];
+// } 
+
