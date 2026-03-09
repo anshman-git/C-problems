5992223e776c759c774644f5d92661be72e104b4
Anshman Khare <anshmankhare@gmail.com>
2026-03-09 22:36:21 +0530

    155

--- daily.c
+++ daily.c
@@ -4001,3 +4001,20 @@
     
 //     return count;
 // }
+
+// 155
+// Remove duplicate from sorted array (leetcode 26)
+// int removeDuplicates(int* nums, int numsSize) {
+//     if(numsSize == 0) return 0;
+
+//     int k = 0;
+
+//     for(int i = 1; i < numsSize; i++) {
+//         if(nums[i] != nums[k]) {
+//             k++;
+//             nums[k] = nums[i];
+//         }
+//     }
+
+//     return k + 1;
+// }
