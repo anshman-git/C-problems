a1ba027d48128c8a65c76240857bae53d1be307d
Anshman Khare <anshmankhare@gmail.com>
2026-03-24 22:28:47 +0530

    167

--- daily.c
+++ daily.c
@@ -4264,3 +4264,22 @@
 //     if(len==26) return true;
 //     return false;
 // }
+
+
+// 167
+// Type of Triangle (leetcode 3024) 
+char* triangleType(int* nums, int numsSize) {
+    if (!((nums[0] + nums[1] > nums[2]) && (nums[0] + nums[2] > nums[1]) && (nums[1] + nums[2] > nums[0]))) {
+        return "none";
+    }
+
+    if (nums[0] == nums[1] && nums[1] == nums[2]) {
+        return "equilateral";
+    } 
+    
+    if (nums[0] == nums[1] || nums[0] == nums[2] || nums[1] == nums[2]) {
+        return "isosceles";
+    }
+
+    return "scalene";
+}
