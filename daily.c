3697890c5835ab157585bb5696eba3a1cad75fa4
Anshman Khare <anshmankhare@gmail.com>
2026-04-04 23:50:38 +0530

    168

--- daily.c
+++ daily.c
@@ -4329,3 +4329,15 @@
 //     }
 //     return a;
 // }
+
+// 168
+// Ugly numbeer (leeetcode 263) 
+// bool isUgly(int n) {
+//     if (n <= 0) return false;
+
+//     while (n % 2 == 0) n /= 2;
+//     while (n % 3 == 0) n /= 3;
+//     while (n % 5 == 0) n /= 5;
+
+//     return n == 1;
+// }
