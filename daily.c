7e80be9099605cc1556d7db54a791fa85c31166c
Anshman Khare <anshmankhare@gmail.com>
2026-04-08 23:50:16 +0530

    173

--- daily.c
+++ daily.c
@@ -4395,3 +4395,18 @@
 //     else return true;
 //     }
 // }
+
+// 173
+// Ransom Note(leetcode 383)
+// bool canConstruct(char* ransomNote, char* magazine) {
+//     int count[26] = {0};
+//     for (int i = 0; magazine[i] != '\0'; i++) {
+//         count[magazine[i] - 'a']++;
+//     }
+//     for (int i = 0; ransomNote[i] != '\0'; i++) {
+//         if (--count[ransomNote[i] - 'a'] < 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
