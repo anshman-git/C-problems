0fd841c0476e89c8f17668adef4e8c08ee01de02
Anshman Khare <anshmankhare@gmail.com>
2026-04-11 23:09:13 +0530

    177

--- daily.c
+++ daily.c
@@ -4457,3 +4457,14 @@
 //     }
 //     return count;
 // }
+
+// 177
+// NUmber Complement
+// int findComplement(int num) {
+//     if (num == 0) return 1;
+//     unsigned int mask = ~0;
+//     while (num & mask) {
+//         mask <<= 1;
+//     }
+//     return num ^ ~mask;
+// }
