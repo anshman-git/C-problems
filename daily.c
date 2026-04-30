5fa273513b0b2dfef8d9719b41d2c681b71cf9f1
Anshman Khare <anshmankhare@gmail.com>
2026-04-30 23:56:20 +0530

    195

--- daily.c
+++ daily.c
@@ -4742,3 +4742,18 @@
 //     *returnSize=numsSize;
 //     return arr;
 // }
+
+// 195
+// Neither minimum nor maximum(leetcode 2733)
+// int findNonMinOrMax(int* nums, int numsSize) {
+//     if(numsSize<3) return -1;
+//     int a=nums[0],b=nums[1],c=nums[2];
+//     if ((a>b && a<c) || (a<b && a>c)) {
+//         return a;
+//     }
+//     if ((b>a && b<c) || (b<a && b>c)) {
+//         return b;
+//     }
+//     return c;
+
+// }
