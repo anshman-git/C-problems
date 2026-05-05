8378fa2ff75a12106d4a779351ea75e39cdff713
Anshman Khare <anshmankhare@gmail.com>
2026-05-05 23:49:01 +0530

    197

--- daily.c
+++ daily.c
@@ -4772,3 +4772,21 @@
 //     }
 //     return s;
 // }
+
+// 197
+// Count item matching a rule
+// int countMatches(char*** items, int itemsSize, int* itemsColSize, char* ruleKey, char* ruleValue) {
+    
+//     int index=0;
+//     int count=0;
+
+//     if (strcmp(ruleKey, "color")==0) {
+//         index=1;
+//     } else if (strcmp(ruleKey, "name") == 0) {
+//         index=2;
+//     }
+//     for (int i=0;i<itemsSize;i++) {
+//         if (strcmp(items[i][index], ruleValue)==0) count++;
+//     }
+//     return count;
+// }
